f79206ed3403cc57e3ba1f09c15e40e5f676b6b9
Fe4ester <xdxdxd4305@gmail.com>
2025-03-01 20:18:43 +0200

    Удалил эндпоинт redoc т.к. используем только swagger

--- SeeOnline/urls.py
+++ SeeOnline/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +10,5 @@ urlpatterns = [
 
     # Подключаем документацию
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
