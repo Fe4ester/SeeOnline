00dbf800a8064dd9833073c25c0883f0e71f045b
lian <xdxdxd4305@gmail.com>
2025-03-22 20:32:27 +0200

    Добавил проверку на существование пользователя, и добавил функцию auth для команды authtracker

--- tracker/services/tracker_service.py
+++ tracker/services/tracker_service.py
@@ -1,25 +1,28 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 from telethon.tl.types import UserStatusOnline, UserStatusOffline
-
-
-# Запросит номер телефона, и код в терминале
-def default_auth(api_id, api_hash):
-    try:
-        with TelegramClient(StringSession(), api_id, api_hash) as client:
-            return client.session.save()
-
-    except Exception as e:
-        print(f"Ошибка авторизации: {e}")
-        return
+from telethon.errors import UsernameNotOccupiedError
 
 
 def check_online(username, api_hash, api_id, session_string):
-    with TelegramClient(StringSession(session_string), api_id, api_hash) as client:
-        user = client.get_entity(username)
-        if isinstance(user.status, UserStatusOnline):
-            return True
-        elif isinstance(user.status, UserStatusOffline):
-            return False
-        else:
-            return
+    try:
+        with TelegramClient(StringSession(session_string), api_id, api_hash) as client:
+            user = client.get_entity(username)
+            if isinstance(user.status, UserStatusOnline):
+                return True
+            elif isinstance(user.status, UserStatusOffline):
+                return False
+            else:
+                return None
+    except UsernameNotOccupiedError:
+        return None
+    except Exception as e:
+        return None
+
+
+def auth(api_id, api_hash):
+    try:
+        with TelegramClient(StringSession(), api_id, api_hash) as client:
+            return client.session.save()
+    except:
+        return None
